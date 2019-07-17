48a4f66c9789460fb6ba25b3a04f0f5d809d4232
Ilja <info@ilja-weber.de>
2019-07-17 13:07:39 +0200

    fix: fix bugs and add new features

--- PostCodeAutocomplete.js
+++ PostCodeAutocomplete.js
@@ -318,6 +318,14 @@ function PostCodeAutocomplete(config) {
             });
         });
 
+        $self.inputElement.addEventListener('focus', function() {
+            var acCall = $self.getPredictions();
+            acCall.then( function($data) {
+                $self.predictions = $data.result.predictions;
+                $self.validate();
+            });
+        });
+
         // Register blur event
         $self.inputElement.addEventListener('blur', function() {
             $self.removeDropdown();
