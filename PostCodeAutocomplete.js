f37bae7d1b6ac83669b498454877c5792a40a990
Ilja <info@ilja-weber.de>
2019-07-23 09:34:43 +0200

    feat: add edge support

--- PostCodeAutocomplete.js
+++ PostCodeAutocomplete.js
@@ -388,7 +388,7 @@ function PostCodeAutocomplete(config) {
         // Register mouse navigation
         $self.inputElement.addEventListener('keydown', function(mEvent) {
             var selectedCityName, cityNameField, event;
-            if ('ArrowUp' === mEvent.code || 'Up' === mEvent.key) {
+            if ('ArrowUp' === mEvent.key || 'Up' === mEvent.key) {
                 mEvent.preventDefault();
 
                 if (0 === $self.activeElementIndex) {
@@ -413,7 +413,7 @@ function PostCodeAutocomplete(config) {
                 $self.renderDropdown();
             }
 
-            if ('ArrowDown' === mEvent.code || 'Down' === mEvent.key) {
+            if ('ArrowDown' === mEvent.key || 'Down' === mEvent.key) {
                 mEvent.preventDefault();
                 if ($self.activeElementIndex < ($self.predictions.length-1)) {
                     $self.activeElementIndex++;
@@ -430,7 +430,7 @@ function PostCodeAutocomplete(config) {
                 $self.renderDropdown();
             }
 
-            if ('Enter' === mEvent.code || 'Enter' === mEvent.key) {
+            if ('Enter' === mEvent.key || 'Enter' === mEvent.key) {
                 mEvent.preventDefault();
 
                 // If only one prediction
