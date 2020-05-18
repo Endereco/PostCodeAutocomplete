68e101827999a0a41aa7a7ce430d1c0ef3266224
Ilja <info@ilja-weber.de>
2020-05-18 15:53:23 +0200

    fix: fix offset calculation

--- PostCodeAutocomplete.js
+++ PostCodeAutocomplete.js
@@ -206,7 +206,7 @@ function PostCodeAutocomplete(config) {
         ul.style.position = 'absolute';
         ul.style.top = 4 + $self.inputElement.offsetTop + $self.inputElement.offsetHeight + 'px';
         if ('rtl' === direction) {
-            ul.style.right = $self.inputElement.offsetParent.offsetWidth - parseFloat(getComputedStyle($self.inputElement.offsetParent).paddingRight) - $self.inputElement.offsetWidth + 'px';
+            ul.style.right = $self.inputElement.offsetParent.offsetWidth - $self.inputElement.offsetLeft - $self.inputElement.offsetWidth + 'px';
         } else {
             ul.style.left = $self.inputElement.offsetLeft + 'px';
         }
